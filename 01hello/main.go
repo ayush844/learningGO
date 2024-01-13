1b489e942d699cd0feaab5aa08e5eeca953f2a53
ayush844 <ayush.xyz1625@gmail.com>
2024-01-13 12:12:44 +0530

    lexer in golang and types

--- 01hello/main.go
+++ 01hello/main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello from ayush")
+	fmt.Println("hey there its Ayush this side")
 }
+
+// LEXAR: job of lexar is to correct our grammar in code
+
+// types in GOLANG
+
+// > it is case sensitive
+// > variable type should be known in advance
+// > almost everything in golang is a type
+
+// String Bool Integer(unit8 unit64 int8 int64 uintptr) Floating(float32 float64) Complex
+//  Array Slices Maps Structs Pointers
+// Function Channels..etc are also treated as types
