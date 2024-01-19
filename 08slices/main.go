8a37f8860baa6ca18dcb3665347347da08b85790
ayush844 <ayush.xyz1625@gmail.com>
2024-01-19 13:14:00 +0530

    slices in golang

---
+++ 08slices/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	fmt.Println("hello")
+
+	//we don't have to tell the size here and we need to initialize it in starting
+	var fruitList = []string{}
+	fmt.Printf("type of fruitlist is %T\n", fruitList)
+	//type of fruitlist is []string
+
+	var newFruitList = []string{"apple", "tomato", "peach"}
+	fmt.Printf("type of fruitlist is %T\n", newFruitList)
+
+	//-------------------------------we can also add new values in the slice-------------------------------
+
+	newFruitList = append(newFruitList, "mango", "banana")
+	fmt.Println(newFruitList)
+	// [apple tomato peach mango banana]
+
+	// newFruitList = append(newFruitList[1:])
+	// fmt.Println(newFruitList)
+	// // [tomato peach mango banana]
+
+	// newFruitList = append(newFruitList[:4])
+	// fmt.Println(newFruitList)
+	// // [apple tomato peach mango]
+
+	//include 1rst element and go till before the last element
+	// newFruitList = append(newFruitList[1:4])
+	// fmt.Println(newFruitList)
+	// // [tomato peach mango]
+
+	highScore := make([]int, 4)
+	highScore[0] = 193
+	highScore[1] = 934
+	highScore[2] = 385
+	highScore[3] = 276
+
+	// highScore[4] = 361 ==> /error/ panic: runtime error: index out of range [4] with length 4
+
+	fmt.Println(highScore)
+	// [193 934 385 276]
+
+	highScore = append(highScore, 111, 555, 999) //as we append more values the entire reallocation of memory take place
+	fmt.Println(highScore)
+	// [193 934 385 276 111 555 999]
+
+	//-------------------------------sorting the slice and other operations-------------------------------
+
+	sort.Ints(highScore) // sorts the entire slice in increasing order
+	fmt.Println(highScore)
+	// [111 193 276 385 555 934 999]
+
+	fmt.Println(sort.IntsAreSorted(highScore)) //it tells if our slice is sorted or not
+	// true
+
+}
