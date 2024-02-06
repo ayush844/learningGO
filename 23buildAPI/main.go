1426b07c9f232bde635c8d502a72c14570187fdb
ayush844 <ayush.xyz1625@gmail.com>
2024-02-06 13:43:45 +0530

    Get one course based on request id

--- 23buildAPI/main.go
+++ 23buildAPI/main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 //model for course - file
@@ -43,8 +45,34 @@ func serveHome(w http.ResponseWriter, r *http.Request){
 	w.Write([]byte("<h1>WELCOME TO API BY AYUSH SHARMA</h1>"))
 }
 
+//get all the courses
 func getAllCourses(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Get all courses")
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(courses)
+}
+
+
+// get a particular course
+func getOneCourse(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Get one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// grab id from request
+	params := mux.Vars(r)
+	fmt.Println(params)
+
+	//loop through courses find matching id and return the response
+
+	for _, course := range courses{
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+
+	json.NewEncoder(w).Encode("no course found of the given id")
+
+	return
+
 }
